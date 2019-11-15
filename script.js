36fc60df2626a35fa532ae406bffe88b27b61677
collijac000 <53783122+collijac000@users.noreply.github.com>
2019-11-15 12:40:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function madlibt(){
+
+
+let Noun1=document.getElementById("noun1").value;
+let Adjective1=document.getElementById("adjective1").value;
+let Noun2=document.getElementById("noun2").value;
+let PName=document.getElementById("pName").value;
+let PVerb1=document.getElementById("pVerb1").value;
+let PVerb2=document.getElementById("pVerb2").value;
+let Noun3=document.getElementById("noun3").value;
+let Noun4=document.getElementById("noun4").value;
+let PVerb3=document.getElementById("pVerb3").value;
+let Adjective2=document.getElementById("adjective2").value;
+let PVerb4=document.getElementById("pVerb4").value;
+
+
+ document.getElementById("madlibs").innerHTML = "<p>There once was a  <span class='words'> " +Noun1+"</span> who had a <span class='words'> "+Adjective1+"</span> <span class='words'>"+Noun2+"</span>. They were so determined to get to <span class='words'>"+PName+"</span> that they<span class='words'> "+PVerb1+"</span>. When they finally got to <span class='words'>"+PName+"</span> they sat down and <span class='words'>"+PVerb2+"</span><span class='words'> "+Noun3+"</span>Then a <span class='words'>"+Noun4+" </span><span class='words'>"+PVerb3+"</span> you to him and asked why are you so <span class='words'>"+Adjective2+"</span>? They replied simply because I <span class='words'>"+PVerb4+".</span></p>"
+
+hey.innerHTML="";
+}
+
